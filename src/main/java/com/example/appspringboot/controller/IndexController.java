5fbcb027a5ed839872312674d475b33ff3108090
root <root@gmail.com>
2019-10-29 15:50:04 -0400

    add index

--- src/main/java/com/example/appspringboot/controller/IndexController.java
+++ src/main/java/com/example/appspringboot/controller/IndexController.java
@@ -1,14 +1,19 @@
 package com.example.appspringboot.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController 
 {
-	@RequestMapping("")
-    public String index()
+	@RequestMapping("app")
+    public Object index()
 	{
-        return ("Hello, SpringBoot on Wildfly");
+		Map<String, Object> map = new HashMap<>();
+		map.put("app status", "app is runnig.");
+        return map;
     }
 }
