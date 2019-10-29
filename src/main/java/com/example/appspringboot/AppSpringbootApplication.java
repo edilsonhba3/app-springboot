646bce1e8b89e6a23e949cd70e772992fb6d98bb
root <root@gmail.com>
2019-10-29 16:01:15 -0400

    add initialiazer

--- src/main/java/com/example/appspringboot/AppSpringbootApplication.java
+++ src/main/java/com/example/appspringboot/AppSpringbootApplication.java
@@ -2,12 +2,21 @@ package com.example.appspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class AppSpringbootApplication 
+public class AppSpringbootApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(AppSpringbootApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+	{
+        return application.sources(AppSpringbootApplication.class);
+    }
 }
